e9c90ee980905a79b9236122e90a87aac856a735
Abdul Rehman <abdul-rehman@myseneca.ca>
2019-01-30 22:05:23 -0600

    built functions, now just need to impliment

--- js/main.js
+++ js/main.js
@@ -18,8 +18,6 @@ $(function () {
             });
     }
 
-    initializeEmployeesModel();
-
     function showGenericModal(title, message) {
         $("#genericModal .modal-title").text(title);
         $("#genericModal .modal-body").text(message);
@@ -29,21 +27,42 @@ $(function () {
     function refreshEmployeeRows(employees) {
         console.log(`inside refreshEmployeeRows`);
         //First try
-        let myTemplate = _.template(`<% _.forEach(employees, function(currentEmp){ %>` + //evaluate
-            + `<div class="row body-row" data-id=" <%-currentEmp._id%>"> ` //escape start
-            + `<div class="col-xs-4 body-column"> <%-currentEmp.FirstName%> </div>`
-            + `<div class="col-xs-4 body-column"><%-currentEmp.LastName%> </div>`
-            + `<div class="col-xs-4 body-column"><%-currentEmp.Position.PositionName%> </div>` //escape end
-            + `<% }); %>` + `</div>`);
+        let myTemplate = _.template(
+            '<% _.forEach(employees, function(employee) { %>' +
+                '<div class="row body-row" data-id=<%- employee._id %>>' + 
+                    '<div class="col-xs-4 body-column"><%- employee.FirstName %></div>' + 
+                    '<div class="col-xs-4 body-column"><%- employee.LastName %></div>' + 
+                    '<div class="col-xs-4 body-column"><%- employee.Position.PositionName %></div>' + 
+                '</div>' +
+            '<% }); %>');
+
         $("employees-table").empty();
-        console.log(employees[0]._id);
-        console.log(employees[0].FirstName);
-        console.log(employees[0].LastName);
-        console.log(employees[0].Position.PositionName);
-        // console.log(myTemplate({'employees' : employees}));
-      //  $("employees-table").append();
-        
+        // console.log(employees[0]._id);
+        // console.log(employees[0].FirstName);
+        // console.log(employees[0].LastName);
+        // console.log(employees[0].Position.PositionName);
+        console.log(myTemplate({'employees' : employees}));
+         myTemplate({'employees' : employees});
+       $("employees-table").append(myTemplate({'employees' : employees}));
     }
+
+    function getFilteredEmployeesModel(filterString)
+    {
+        let temp = _.filter(employees,function(currentEmp){
+            return currentEmp.FirstName == filterString || currentEmp.LastName == filterString || currentEmp.Position.PositionName == filterString;
+        });
+    }
+
+    function getEmployeeModelById(id) {
+        let temp = _.filter(employees,function(currentEmp){
+            return currentEmp._id == id;
+        });
+
+        let deepArr = _.cloneDeep(temp);
+        return deepArr;
+    }
+
+    initializeEmployeesModel();
 
     //Assignment 2 ends
 });
