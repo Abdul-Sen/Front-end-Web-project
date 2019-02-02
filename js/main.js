1864009be4faeff8bc0eceeda3e1d62df0a51da6
Abdul Rehman <abdul-rehman@myseneca.ca>
2019-02-01 18:36:19 -0600

    Assignment 2 completed

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 $(function () {
-    console.log(`hello again world!`);
     //Assignment 2 starts
     let employeesModel = [];
     function initializeEmployeesModel() {
@@ -19,42 +18,41 @@ $(function () {
     }
 
     function showGenericModal(title, message) {
-        $("#genericModal .modal-title").text(title);
-        $("#genericModal .modal-body").text(message);
-        $("#genericModal").modal('show');
+        // $("#genericModal").empty(); //TODO: WHY IS THIS NOT WORKING?
+        $("#genericModal .modal-title").html(title);
+        $("#genericModal .modal-body").html(message);
+        $("#genericModal").modal("show");
     }
 
     function refreshEmployeeRows(employees) {
-        console.log(`inside refreshEmployeeRows`);
         //First try
         let myTemplate = _.template(
             '<% _.forEach(employees, function(employee) { %>' +
-                '<div class="row body-row" data-id=<%- employee._id %>>' + 
-                    '<div class="col-xs-4 body-column"><%- employee.FirstName %></div>' + 
-                    '<div class="col-xs-4 body-column"><%- employee.LastName %></div>' + 
-                    '<div class="col-xs-4 body-column"><%- employee.Position.PositionName %></div>' + 
-                '</div>' +
+            '<div class="row body-row" data-id=<%- employee._id %>>' +
+            '<div class="col-xs-4 body-column"><%- employee.FirstName %></div>' +
+            '<div class="col-xs-4 body-column"><%- employee.LastName %></div>' +
+            '<div class="col-xs-4 body-column"><%- employee.Position.PositionName %></div>' +
+            '</div>' +
             '<% }); %>');
 
         $("#employees-table").empty();
-        // console.log(employees[0]._id);
-        // console.log(employees[0].FirstName);
-        // console.log(employees[0].LastName);
-        // console.log(employees[0].Position.PositionName);
-        // console.log(myTemplate({'employees' : employees}));
-         myTemplate({'employees' : employees});
-       $("#employees-table").append(myTemplate({'employees' : employees}));
+        myTemplate({ 'employees': employees });
+        $("#employees-table").append(myTemplate({ 'employees': employees }));
     }
 
-    function getFilteredEmployeesModel(filterString)
+    function getFilteredEmployeesModel(filterString)//TODO: Finish this
     {
-        let temp = _.filter(employees,function(currentEmp){
-            return currentEmp.FirstName == filterString || currentEmp.LastName == filterString || currentEmp.Position.PositionName == filterString;
+        return _.filter(employeesModel, function (currentEmp) {
+            if ((currentEmp.FirstName.toUpperCase()).includes(filterString.toUpperCase())
+                || (currentEmp.LastName.toUpperCase()).includes(filterString.toUpperCase())
+                || (currentEmp.Position.PositionName.toUpperCase()).includes(filterString.toUpperCase())) {
+                return currentEmp;
+            }
         });
     }
 
     function getEmployeeModelById(id) {
-        let temp = _.filter(employees,function(currentEmp){
+        let temp = _.filter(employeesModel, function (currentEmp) {
             return currentEmp._id == id;
         });
 
@@ -63,6 +61,28 @@ $(function () {
     }
 
     initializeEmployeesModel();
+
+    $("#employee-search").keyup(function () {
+
+        refreshEmployeeRows(getFilteredEmployeesModel((this).value)); //refreshes row /w a given string
+    });
+
+    $("#employees-table").on("click", ".row", function () { // watch the whole document for when existing (or new) tr elements are clicked
+        let result = getEmployeeModelById($(this).attr("data-id"));
+
+        result[0].HireDate = moment(result[0].HireDate).format("LL"); //creating a moment obj, then formating date, then setting that date to the current obj
+        let bodyTemplate = _.template(
+            '<strong>Address:</strong>'+
+            '<%- employee.AddressStreet %> <%- employee.AddressCity %>, <%- employee.AddressState %> <%-employee.AddressZip %>'+
+            '<br> <strong>Phone Number:</strong>'+
+            '<%- employee.PhoneNum %> ext: <%-employee.Extension%><br>'+
+            '<strong>Hire Date:</strong>'+
+            '<%- employee.HireDate %>');
+
+        let templateBody = bodyTemplate({ 'employee': result[0] });
+        showGenericModal(result[0].FirstName + " " + result[0].LastName, templateBody);
+    });
+
 
     //Assignment 2 ends
 });
